48f106efe5e58901690028e7140ec3d6c4cfe0cc
nogu66 <ezochi66@gmail.com>
2023-05-27 10:59:27 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by YutaNoguchi on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
